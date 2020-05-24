3188bcf88c16f0fb6064a43d8e01c4712eccbfaf
Sethu Pavan <sethupavan12@gmail.com>
2020-05-24 19:44:27 +0100

    Update wow.js
    disable others stealing code but theres one trick

--- wow.js
+++ wow.js
@@ -1,3 +1,11 @@
+$(document).bind("contextmenu",function(e) {
+ e.preventDefault();
+});
+$(document).keydown(function(e){
+    if(e.which === 123){
+       return false;
+    }
+});
 
 $(document).ready(function () {
   var input = $('.field').find('input, textarea');
